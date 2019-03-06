a4ea2457af046799516793466132738568f22dfb
Brian Francisco <ibrianfrancisco@gmail.com>
2019-03-05 23:26:18 -0800

    added readme and sourcemaps

---
+++ README.md
@@ -0,0 +1,32 @@
+# Gulp Setup
+
+#### Description
+My gulp setup used to create and design static sites quickly. Tasks that are automated: 
+
+- Compiling SCSS to CSS and ES6 to JS
+- Soft refresh when files are changed
+- Minifying CSS and JS files
+- Renaming minified files
+
+#### Modules Used:
+
+- Gulp
+- Browser Sync
+- Browserify
+- Babel
+- Sass
+- Sourcemaps
+- Uglify
+- Gulp Rename
+
+#### Getting Started
+
+    git clone https://github.com/ibrianfrancisco/gulp-setup.git
+    cd gulp-setup
+    npm install
+    
+    // Run local environment
+    gulp start
+
+### Future updates
+* Add image optimizer
--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,9 @@ const uglify = require('gulp-uglify');
 const rename = require('gulp-rename');
 const browserSync = require('browser-sync');
 const browserify = require('browserify');
+const sourcemaps = require('gulp-sourcemaps');
 
-// Spin up a local web server on localhost:3000
+// Spin up a local dev server on localhost:3000
 function serve() {
   return browserSync.init({
     server: 'build',
@@ -15,7 +16,7 @@ function serve() {
   });
 }
 
-// Watch for changes in app directory
+// Watch for changes in app directory. Hot reloading only for html
 function watch() {
   gulp.watch('app/scripts/*.js', processJs);
   gulp.watch([
@@ -25,7 +26,7 @@ function watch() {
   gulp.watch('app/index.html', processHtml).on('change', browserSync.reload);
 }
 gulp.task('watch', watch);
-gulp.task('start', gulp.series(processImgs, processHtml, processJs, processCss, gulp.parallel(serve, watch)));
+gulp.task('start', gulp.series(processImgs, processHtml, processCss, processJs, gulp.parallel(serve, watch)));
 
 
 function processImgs() {
@@ -41,9 +42,10 @@ function processHtml() {
 }
 gulp.task('processHtml', processHtml);
 
-
+// maps scss file, compiles and minifies
 function processCss() {
   return gulp.src('app/styles/main.scss')
+    .pipe(sourcemaps.init())
     .pipe(sass({
       errorLogToConsole: true,
       outputStyle: 'compressed'
@@ -52,14 +54,16 @@ function processCss() {
     .pipe(rename({
       suffix: '.min'
     }))
+    .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('build/styles'))
     .pipe(browserSync.stream());
 }
 gulp.task('processCss', processCss);
 
-
+// maps js file, compiles and minifies
 function processJs() {
   return gulp.src('app/scripts/*.js')
+    .pipe(sourcemaps.init())
     .pipe(babel({
       presets: ['env']
     }))
@@ -67,7 +71,9 @@ function processJs() {
     .pipe(rename({
       suffix: '.min'
     }))
+    .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('build/scripts'))
+
     .pipe(browserSync.stream());
 }
 gulp.task('processJs', processJs);
