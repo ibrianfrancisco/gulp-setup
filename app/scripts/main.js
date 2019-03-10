1c3efa2db2c858e07142026d9c2d3d36650265bf
Brian Francisco <ibrianfrancisco@gmail.com>
2019-03-10 01:23:29 -0800

    redesigned front page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,3 @@
 window.addEventListener('load', () => {
-  document.getElementById('main').insertAdjacentHTML(
-    'afterbegin',
-    '<section class="card welcome"><h3>Welcome</h3></section>'
-  );
+  console.log('hello');
 });
